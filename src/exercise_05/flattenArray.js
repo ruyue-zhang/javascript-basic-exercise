3139d95a996002d4c832bbfafc256edc595ce1e1
lpdft <772346730@qq.com>
2020-09-09 12:42:03 +0800

    [ruyue] feat:pass flatten_array_spec

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,19 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Flatten undefined or null array');
+  } else if (array.length === 0) {
+    return [];
+  } else {
+    const flattenArray = [];
+    Object.keys(array).forEach((key) => {
+      if (Array.isArray(array[key])) {
+        flattenArray.push(...array[key]);
+      } else {
+        flattenArray.push(array[key]);
+      }
+    });
+    return flattenArray;
+  }
 }
