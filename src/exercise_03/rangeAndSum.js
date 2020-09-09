a63c4f4cf0f3cf2a0b6d287636bab69bcae1192b
lpdft <772346730@qq.com>
2020-09-09 11:19:46 +0800

    [ruyue] feat:pass range_and_array_spec

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,17 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  const rangeArray = [];
+  if (start <= end) {
+    for (let index = start; index < end; index += 1) {
+      rangeArray.push(index);
+    }
+  } else {
+    for (let index = start; index > end; index -= 1) {
+      rangeArray.push(index);
+    }
+  }
+  return rangeArray;
 }
 
 export function sum(...numbers) {
@@ -17,6 +27,5 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  return (numbers.length > 0) ? numbers.reduce((pre, cur) => pre + cur) : 0;
 }
